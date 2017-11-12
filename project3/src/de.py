093caef948c5aeb0645ebd8408cf97bb0d56fbd8
Unknown <henry.king132@gmail.com>
2017-11-11 19:57:21 -0700

    we tab people now

--- project3/src/de.py
+++ project3/src/de.py
@@ -3,148 +3,148 @@ import random
 import helpers
 
 class DE(EA):
-    
-    def __init__(self, shape, mu, beta, proboRecombo):
-        #super(shape, max)
-        super().__init__(shape, mu)
-        self.parents = []
-        self.beta = beta
-        self.pr = proboRecombo
+	
+	def __init__(self, shape, mu, beta, proboRecombo):
+		#super(shape, max)
+		super().__init__(shape, mu)
+		self.parents = []
+		self.beta = beta
+		self.pr = proboRecombo
 
 
-    def crossOver(self, parent, trialVec):
-        #use the trial vectors to create offspring
-        mask = []
-        spawn = []
-        #uniform crossover between parent and trial vector
-        #build mask
-        for i in range(len(parent)):
-            mask.append(random.randint(0, 1))
+	def crossOver(self, parent, trialVec):
+		#use the trial vectors to create offspring
+		mask = []
+		spawn = []
+		#uniform crossover between parent and trial vector
+		#build mask
+		for i in range(len(parent)):
+			mask.append(random.randint(0, 1))
 
-        #construct child
-        for i in range(len(parent)):
-            if mask[i] == 1:
-                spawn.append(parent[i])
-            else:
-                spawn.append(trialVec[i])
-        #print(len(spawn), spawn[0:2])
-        return(spawn)
+		#construct child
+		for i in range(len(parent)):
+			if mask[i] == 1:
+				spawn.append(parent[i])
+			else:
+				spawn.append(trialVec[i])
+		#print(len(spawn), spawn[0:2])
+		return(spawn)
 
-    def mutate(self, parent, best):
-        dudes =[]
+	def mutate(self, parent, best):
+		dudes =[]
 
-        #parent (xi)
-        xi = parent
-        dudes.append(xi)
-        
-        #select the target, the best individual (x1)
-        if(xi == best):
-            x1 = self.pop[-2]
-            dudes.append(x1)
-        else:
-            x1 = best
-            dudes.append(x1)
-        
-        #randomly select two different(x2,x3 !=x1, xi)
-        x2 = self.grabDude(dudes)
-        dudes.append(x2)
-        x3 = self.grabDude(dudes)
-        dudes.append(x3)
-        # print("xi " + str(xi[0:2]))
-        # print("x1 " + str(x1[0:2]))
-        # print("x2 " + str(x2[0:2]))
-        # print("x3 " + str(x3[0:2]))
-        #create trial vector, append to list of trial vectors
-        ut=[]
-        for i in range(len(xi)):
-            ut.append(x1[i] + self.beta*(x2[i]-x3[i]))
-        #print(len(ut), ut[0:3])
-        return(ut)
+		#parent (xi)
+		xi = parent
+		dudes.append(xi)
+		
+		#select the target, the best individual (x1)
+		if(xi == best):
+			x1 = self.pop[-2]
+			dudes.append(x1)
+		else:
+			x1 = best
+			dudes.append(x1)
+		
+		#randomly select two different(x2,x3 !=x1, xi)
+		x2 = self.grabDude(dudes)
+		dudes.append(x2)
+		x3 = self.grabDude(dudes)
+		dudes.append(x3)
+		# print("xi " + str(xi[0:2]))
+		# print("x1 " + str(x1[0:2]))
+		# print("x2 " + str(x2[0:2]))
+		# print("x3 " + str(x3[0:2]))
+		#create trial vector, append to list of trial vectors
+		ut=[]
+		for i in range(len(xi)):
+			ut.append(x1[i] + self.beta*(x2[i]-x3[i]))
+		#print(len(ut), ut[0:3])
+		return(ut)
 
-    #get a guy that ain't in dudes
-    def grabDude(self, dudes):
-            while(True):
-                #random position in population
-                x = self.pop[random.randint(0,len(self.pop)-1)]
-                if x in dudes:
-                    continue
-                else:
-                    return(x)
+	#get a guy that ain't in dudes
+	def grabDude(self, dudes):
+			while(True):
+				#random position in population
+				x = self.pop[random.randint(0,len(self.pop)-1)]
+				if x in dudes:
+					continue
+				else:
+					return(x)
 
-    def selectFrom(self):
-        pass
+	def selectFrom(self):
+		pass
 
-    def train(self, x, y, valX, valY, maxGen):
-        ###Initialize###
-        super().train() # sets up some vars
-        #mark the time step, or generation
-        t = 0
-        self.fitness={}
-        self.fitnext={}
+	def train(self, x, y, valX, valY, maxGen):
+		###Initialize###
+		super().train() # sets up some vars
+		#mark the time step, or generation
+		t = 0
+		self.fitness={}
+		self.fitnext={}
 
-        #evaluate the fitness of the population
-        for i in range(len(self.pop)):
-            self.fitness.update({i : self.evaluateFitness(self.pop[i], x, y)})
-        #sort by fitness
-        self.sortFit = sorted(self.fitness.items(), key=lambda x:x[1])
-        
-        best = max(self.fitness, key=(lambda key: self.fitness[key]))
-        print ("Starting Best at "+str(best) + " with fitness ", str(self.fitness[best]))
+		#evaluate the fitness of the population
+		for i in range(len(self.pop)):
+			self.fitness.update({i : self.evaluateFitness(self.pop[i], x, y)})
+		#sort by fitness
+		self.sortFit = sorted(self.fitness.items(), key=lambda x:x[1])
+		
+		best = max(self.fitness, key=(lambda key: self.fitness[key]))
+		print ("Starting Best at "+str(best) + " with fitness ", str(self.fitness[best]))
 
-        #sort population by fitness
-        self.sortPop = sorted(self.pop, key=lambda j:self.evaluateFitness(j, x, y))
-        self.pop = self.sortPop
+		#sort population by fitness
+		self.sortPop = sorted(self.pop, key=lambda j:self.evaluateFitness(j, x, y))
+		self.pop = self.sortPop
 
-        #print(self.evaluateFitness(self.sortPop[-1],x,y))
-        #print(self.evaluateFitness(self.pop[-1],x,y))
-        #print(len(self.sortPop))
-        self.bestEva = [self.pop[-1], self.evaluateFitness(self.pop[-1], x, y)]
-        
-        ##Loop###
-        converged = False
-        while(t <= maxGen and not converged):
-            #nextGen
-            t = t +1
-            newPop = []
+		#print(self.evaluateFitness(self.sortPop[-1],x,y))
+		#print(self.evaluateFitness(self.pop[-1],x,y))
+		#print(len(self.sortPop))
+		self.bestEva = [self.pop[-1], self.evaluateFitness(self.pop[-1], x, y)]
+		
+		##Loop###
+		converged = False
+		while(t <= maxGen and not converged):
+			#nextGen
+			t = t +1
+			newPop = []
 
-            #store current best individual
-            best = self.pop[-1]
+			#store current best individual
+			best = self.pop[-1]
 
-            #for each individual
-            for i in range(len(self.pop)):
-                guy = self.pop[i]
-                #evaluate fitness
-                fit = self.evaluateFitness(guy, x, y)
-                # print(fit)
-                #create trial vector, mutate
-                tv = self.mutate(guy, best)
-                #create the offspring, crossover
-                offSpring = self.crossOver(guy, tv)
-                newFit = self.evaluateFitness(offSpring, x, y)
-                #if child better than parent
-                if newFit > fit:
-                    #add child to nextPop
-                    newPop.append(offSpring)
-                #else add parent to nextPop
-                else:
-                    newPop.append(guy)
+			#for each individual
+			for i in range(len(self.pop)):
+				guy = self.pop[i]
+				#evaluate fitness
+				fit = self.evaluateFitness(guy, x, y)
+				# print(fit)
+				#create trial vector, mutate
+				tv = self.mutate(guy, best)
+				#create the offspring, crossover
+				offSpring = self.crossOver(guy, tv)
+				newFit = self.evaluateFitness(offSpring, x, y)
+				#if child better than parent
+				if newFit > fit:
+					#add child to nextPop
+					newPop.append(offSpring)
+				#else add parent to nextPop
+				else:
+					newPop.append(guy)
 
-            #generational replacement
-            self.pop = newPop
-            
-            #sort population by fitness
-            self.sortPop = sorted(self.pop, key=lambda j:self.evaluateFitness(j, x, y))
-            self.pop = self.sortPop
+			#generational replacement
+			self.pop = newPop
+			
+			#sort population by fitness
+			self.sortPop = sorted(self.pop, key=lambda j:self.evaluateFitness(j, x, y))
+			self.pop = self.sortPop
 
-            best = [self.pop[-1], self.evaluateFitness(self.pop[-1], x, y)]
+			best = [self.pop[-1], self.evaluateFitness(self.pop[-1], x, y)]
 
-            if best[1] > self.bestEva[1] and t > 2:
-                self.bestEva = best
-                print ("Current Best at fitness " + str(best[1]) + " after " + str(t) +" generations with beta at " + str(self.beta))
-                print("Training Error: " + str(self.trainingErrors[-1]) + "\tValidation Error: " + str(self.validationErrors[-1]))
-            if t % 30 ==0:
-                self.beta = self.beta +.1
-            #check convergence
-            converged = self.postIterationProcess(valX, valY, best[1])
+			if best[1] > self.bestEva[1] and t > 2:
+				self.bestEva = best
+				print ("Current Best at fitness " + str(best[1]) + " after " + str(t) +" generations with beta at " + str(self.beta))
+				print("Training Error: " + str(self.trainingErrors[-1]) + "\tValidation Error: " + str(self.validationErrors[-1]))
+			if t % 30 ==0:
+				self.beta = self.beta +.1
+			#check convergence
+			converged = self.postIterationProcess(valX, valY, best[1])
 
-            
+			
--- project3/src/ga.py
+++ project3/src/ga.py
@@ -5,167 +5,167 @@ import random
 import helpers
 
 class GA(EA):
-    
-    def __init__(self, shape, mu):
-        #super(shape, max)
-        super().__init__(shape, mu)
-        self.parents = []
-        
-    def check(self):
-        print(self.shape, self.trueShape, len(self.pop[1]), self.pop[1][1:10])
-        
-    
-    def train(self, x, y, valX, valY, maxGen):
-        super().train() # sets up some vars
-        #mark the time step, or generation
-        t = 0
-        
-        self.fitness={}
-        self.fitnext={}
+	
+	def __init__(self, shape, mu):
+		#super(shape, max)
+		super().__init__(shape, mu)
+		self.parents = []
+		
+	def check(self):
+		print(self.shape, self.trueShape, len(self.pop[1]), self.pop[1][1:10])
+		
+	
+	def train(self, x, y, valX, valY, maxGen):
+		super().train() # sets up some vars
+		#mark the time step, or generation
+		t = 0
+		
+		self.fitness={}
+		self.fitnext={}
 
-        #evaluate the fitness of the population
-        #need to establish train_set, validation_set#######
-        for i in range(len(self.pop)):
-            self.fitness.update({i : self.evaluateFitness(self.pop[i], x, y)})
-        
-        #sort by fitness
-        self.sortFit = sorted(self.fitness.items(), key=lambda x:x[1])
-        
-        #store current best individual
-        self.bestEva = [self.pop[-1], self.evaluateFitness(self.pop[-1], x, y)]
-        best = max(self.fitness, key=(lambda key: self.fitness[key]))
-        print ("Starting Best at "+str(best) + " with fitness ", str(self.fitness[best]))
+		#evaluate the fitness of the population
+		#need to establish train_set, validation_set#######
+		for i in range(len(self.pop)):
+			self.fitness.update({i : self.evaluateFitness(self.pop[i], x, y)})
+		
+		#sort by fitness
+		self.sortFit = sorted(self.fitness.items(), key=lambda x:x[1])
+		
+		#store current best individual
+		self.bestEva = [self.pop[-1], self.evaluateFitness(self.pop[-1], x, y)]
+		best = max(self.fitness, key=(lambda key: self.fitness[key]))
+		print ("Starting Best at "+str(best) + " with fitness ", str(self.fitness[best]))
 
-        converged = False
-        while(t <= maxGen and not converged):
-            #create test_fold
-            
-            t = t +1
-            self.selectFrom()
-            offSpring = self.crossOver()
-            newPop = self.mutate(offSpring)
-            #print(offSpring)
+		converged = False
+		while(t <= maxGen and not converged):
+			#create test_fold
+			
+			t = t +1
+			self.selectFrom()
+			offSpring = self.crossOver()
+			newPop = self.mutate(offSpring)
+			#print(offSpring)
 
-            #add each child to population
-            for i in range(len(newPop)):
-                self.pop.append(newPop[i])
-                        
-            #new pop list
-            self.sortPop = sorted(self.pop, key=lambda j:self.evaluateFitness(j, x, y))
-            self.pop = self.sortPop[len(newPop):] # elitist replacement
-            
-            #update
-            for i in range(len(self.pop)):
-                self.fitness.update({i : self.evaluateFitness(self.pop[i], x, y)})          
-            
-            #new sortFit list
-            self.sortFit = sorted(self.fitness.items(), key=lambda x:x[1])
+			#add each child to population
+			for i in range(len(newPop)):
+				self.pop.append(newPop[i])
+						
+			#new pop list
+			self.sortPop = sorted(self.pop, key=lambda j:self.evaluateFitness(j, x, y))
+			self.pop = self.sortPop[len(newPop):] # elitist replacement
+			
+			#update
+			for i in range(len(self.pop)):
+				self.fitness.update({i : self.evaluateFitness(self.pop[i], x, y)})          
+			
+			#new sortFit list
+			self.sortFit = sorted(self.fitness.items(), key=lambda x:x[1])
  
-            
-            #store current best individual
-            best = [self.pop[-1], self.evaluateFitness(self.pop[-1], x, y)]
+			
+			#store current best individual
+			best = [self.pop[-1], self.evaluateFitness(self.pop[-1], x, y)]
 
-            if best[1] > self.bestEva[1] and t > 2:
-                self.bestEva = best
-                print ("Current Best at fitness " + str(best[1]) + " after " + str(t) +" generations")
-                print("Training Error: " + str(self.trainingErrors[-1]) + "\tValidation Error: " + str(self.validationErrors[-1]))
-            
-            #print(self.sortFit[-1][1])
-            self.postIterationProcess(valX, valY, best[1])
-            
-            
-    
-    #select the parents from the population
-    #rank based
-    #bugs, never picking the worst or best individual
-    def selectFrom(self):
-        self.parents = helpers.rankBasedSelection(self.pop, self.sortFit, 2)
-        
-    #generate offspring according to the crossover rate
-    #global, uniform
-    # should return a whole new copy of the population
-    # example: self.pop = self.crossOver()
-    def crossOver(self):
-        super().crossOver()
-        
-        offspring=[]
-        x1 = self.pop[self.parents[0][0]]
-        #for each other parent, pair it with x1
-        for i in range(1, len(self.parents)):
-            offspring.extend(self.uniCross(x1, self.pop[self.parents[i][0]]))
-        
-        return(offspring)
-        
-    #given two parents, create two offspring using a binary mask
-    def uniCross(self, x1, x2):
-        spawn = []
-        goodTwin = []
-        evilTwin = []
-        mask =[]
-       #chromLength = len(self.pop[self.parents[0][0]])
-        chromLength = len(x1)
-        #x2 = self.pop[self.parents[1][0]]
-        #print(x1[0:2])
-        #print(x2[0:2])
-        
-        #build binary mask
-        for i in range(chromLength):
-            mask.append(random.randint(0, 1))
-        #print(mask[0:2])
-    
-        #create first offspring
-        for i in range(chromLength):
-            if mask[i] == 1:
-                goodTwin.append(x1[i])
-            else:
-                goodTwin.append(x2[i])
-        spawn.append(goodTwin)
-        #create second offspring     
-        for i in range(chromLength):
-            if mask[i] == 1:
-                evilTwin.append(x2[i])
-            else:
-                evilTwin.append(x1[i])
-        spawn.append(evilTwin)
-        
-        #print(spawn[0][0:2])
-        return spawn
-        
+			if best[1] > self.bestEva[1] and t > 2:
+				self.bestEva = best
+				print ("Current Best at fitness " + str(best[1]) + " after " + str(t) +" generations")
+				print("Training Error: " + str(self.trainingErrors[-1]) + "\tValidation Error: " + str(self.validationErrors[-1]))
+			
+			#print(self.sortFit[-1][1])
+			self.postIterationProcess(valX, valY, best[1])
+			
+			
+	
+	#select the parents from the population
+	#rank based
+	#bugs, never picking the worst or best individual
+	def selectFrom(self):
+		self.parents = helpers.rankBasedSelection(self.pop, self.sortFit, 2)
+		
+	#generate offspring according to the crossover rate
+	#global, uniform
+	# should return a whole new copy of the population
+	# example: self.pop = self.crossOver()
+	def crossOver(self):
+		super().crossOver()
+		
+		offspring=[]
+		x1 = self.pop[self.parents[0][0]]
+		#for each other parent, pair it with x1
+		for i in range(1, len(self.parents)):
+			offspring.extend(self.uniCross(x1, self.pop[self.parents[i][0]]))
+		
+		return(offspring)
+		
+	#given two parents, create two offspring using a binary mask
+	def uniCross(self, x1, x2):
+		spawn = []
+		goodTwin = []
+		evilTwin = []
+		mask =[]
+	   #chromLength = len(self.pop[self.parents[0][0]])
+		chromLength = len(x1)
+		#x2 = self.pop[self.parents[1][0]]
+		#print(x1[0:2])
+		#print(x2[0:2])
+		
+		#build binary mask
+		for i in range(chromLength):
+			mask.append(random.randint(0, 1))
+		#print(mask[0:2])
+	
+		#create first offspring
+		for i in range(chromLength):
+			if mask[i] == 1:
+				goodTwin.append(x1[i])
+			else:
+				goodTwin.append(x2[i])
+		spawn.append(goodTwin)
+		#create second offspring     
+		for i in range(chromLength):
+			if mask[i] == 1:
+				evilTwin.append(x2[i])
+			else:
+				evilTwin.append(x1[i])
+		spawn.append(evilTwin)
+		
+		#print(spawn[0][0:2])
+		return spawn
+		
 
-    #mutate the offspring according to the mutation rate
-    # should return a whole new copy of the population
-    # example: self.pop = self.mutate()
+	#mutate the offspring according to the mutation rate
+	# should return a whole new copy of the population
+	# example: self.pop = self.mutate()
 
-    def mutate(self, specimens):
-        super().mutate()
-        #probability of mutation on a feature
-        Pm =.1
-        check = []
-        for i in range(len(specimens)):
-            for j in range(len(specimens[i])):
-                num = random.random()
-                if(num < Pm):
-                    specimens[i][j] = random.normalvariate(specimens[i][j], 1)
-                    check.append(("yes", (i, j)))
-                else:
-                    check.append(("no", (i, j)))
-            #print(check)
-        return(specimens)
-                    
-    #find the slackers and replace them with new dudes
-    def replace(self, newPop, fitNext):
-        for i in range(len(newPop)-1):
-            pass
+	def mutate(self, specimens):
+		super().mutate()
+		#probability of mutation on a feature
+		Pm =.1
+		check = []
+		for i in range(len(specimens)):
+			for j in range(len(specimens[i])):
+				num = random.random()
+				if(num < Pm):
+					specimens[i][j] = random.normalvariate(specimens[i][j], 1)
+					check.append(("yes", (i, j)))
+				else:
+					check.append(("no", (i, j)))
+			#print(check)
+		return(specimens)
+					
+	#find the slackers and replace them with new dudes
+	def replace(self, newPop, fitNext):
+		for i in range(len(newPop)-1):
+			pass
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-                
-            
-    
+		
+		
+		
+		
+		
+		
+		
+		
+		
+				
+			
+	
