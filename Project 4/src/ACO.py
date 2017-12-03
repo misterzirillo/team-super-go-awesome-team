fc5cca8d52917fd29a22dab30d974e140e04861d
Unknown <henry.king132@gmail.com>
2017-12-02 17:07:24 -0700

    more ants

--- Project 4/src/ACO.py
+++ Project 4/src/ACO.py
@@ -6,7 +6,7 @@ import random
 from _overlapped import NULL
 
 class Aco():
-    #2D grid
+    #2D grid, holds tuples (item at position, ant at position)
     grid=[]
     
     #all ants, {ant:(x_pos,ypos,unladen, item)}
@@ -29,7 +29,6 @@ class Aco():
                 j = random(0,grid_dim)
                 k = random(0,grid_dim)
                 if(self.grid[j][k] is None):
-                    
                     colony.update({i:{(j,k,False, None)}})#{ant:(x_pos,ypos,unladen, item)}
                     break 
                 
@@ -55,7 +54,7 @@ class Aco():
                 #if the pick up rate is higher than some random number
                 if r <= pick_up:
                     #pick dat shit up
-                    self.colony.update({ant:ant_x, ant_y, True, curr_item})
+                    self.colony.update({ant:(ant_x, ant_y, True, curr_item)})
                     self.grid[ant_x,ant_y] = None
             else:
                 #if the ant carrying an item and the current site is empty
@@ -67,7 +66,7 @@ class Aco():
                     r = random.uniform(0,1)
                     if r <= drop:
                         #drop dat shit
-                        self.colony.update({ant:ant_x, ant_y, False,None})
+                        self.colony.update({ant:(ant_x, ant_y, False,None)})
                         self.grid[ant_x, ant_y] = curr_item
                                   
             #move ant to a randomly selected neighboring site not occupied by another ant
@@ -76,16 +75,17 @@ class Aco():
             x=[ant_x-1, ant_x, ant_x+1]
             y=[ant_y-1, ant_y, ant_y+1]
             moves=np.permutation(x,y)
-            while(True)
+            while True:
             #randomly pick one and move ant to it
                 move = random.sample(moves)
-                spot=self.grid[move[0]][move[1]
+                spot = self.grid[move[0]][move[1]]
                 if spot[1] == None:
                     self.colony.update({ant:(move[0],move[1],curr_ant[2], curr_ant[3])})
                     break
             
                 
-                
+    #the local density of a data vector within the ants neighborhood
+    #equation 17.45 in engelbrecht            
     def local_density(self,item):
         dens = []
         for y_b in neighborhood:
@@ -96,12 +96,14 @@ class Aco():
         else:
             return(0)
         
-        
+    #computing the pick up probability 
+    #equation 17.46 in engelbrecht 
     def pick_up_rate(self, item):
         pick_up = (gamma1 / (gamma1 + self.local_density(item)))**2
         return(pick_up) 
     
-    
+    #computing the drop probability
+    #equation 17.47 in engelbrecht
     def drop_rate(self, item):
         dens = self.local_density(item)
         if dens < gamma2
@@ -110,77 +112,7 @@ class Aco():
             drop = 1
         return drop
     
+    #
     def build_grid(self):
         pass
-        
-#     #process for deciding which path to take at each node    
-#     #currently shows SACO behavior
-#     def decision(self, node):
-#         
-#         #grab a random probability
-#         r = random.rand(0,1)
-#         for link in node:
-#             #calculate Pa using transProb()
-#             Pa=self.transProb()
-# 
-#             if(r <= Pa):
-#                 return link #follow that path
-#         pass
-#     
-#     def transProb(self):
-#         pass
-#     
-#     #pheromone evaporation
-#     def evap(self):
-#         pass
-#     
-#     def updatePheromone(self):
-#         pass
-#     
-#     def findMin(self):
-#         pass
-#     
-#     #Send out swarm, optimize path, currently looking for minimum path
-#     def search(self):
-#         #set time marker to zero
-#         t = 0
-#         
-#         while(True):
-#             #holds a path for each ant
-#             x_kt = [[]]*len(self.colony)
-#             for ant in self.colony:
-#                 #construct a path x_kt for the ant
-#                 x_kt[ant]= []
-#                 while(True):
-#                     #Select next node 
-#                     link = self.decision()
-#                     x_kt[ant].append(link)
-#                     if(self.destination in x_kt[ant]):
-#                         break
-#                 #Remove all loops from path x_kt
-#                 
-#                 #calculate path length of x_kt
-#                 
-#             for link in self.v:
-#                 #pheromone evaporation
-#                 self.evap()
-#             
-#             for ant in colony:
-#                 for link in x_kt[ant]:
-#                     #update pheromone
-#                     self.updatePheromone()
-#             t =+ 1
-# 
-#             #check stopping condition
-#             #terminate when either maxIter is passed or 
-#             #acceptable solution has been found, findMine(x_kt) < epsilon
-#             if(stop):
-#                 #find min_path
-#                 min_path = self.findMin(x_kt)
-#                 return(min_path)
-#             
-#             
-#             
-#                 
-                
-        
+    
