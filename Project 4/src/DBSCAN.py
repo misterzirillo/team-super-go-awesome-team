4c14615445e88e5bbc1af2e132315c63023175bf
Unknown <henry.king132@gmail.com>
2017-12-08 22:40:18 -0700

    DBSCAN appears functional

--- Project 4/src/DBSCAN.py
+++ Project 4/src/DBSCAN.py
@@ -5,24 +5,31 @@ import random
 
 class DBSCAN():
     
-    data = {}
+    data = []
     min_pts = 0
     radius = 0
     labels = {}
     eps = 0
     
-    def __init__(self, data, radius, min_pts):
+    keys = {}
+    def __init__(self, data, eps, min_pts):
         self.data = data
-        self.radius =radius
+        self.eps =eps
         self.min_pts = min_pts
+        
+        #make an integer key to represent data point
+        for i in range(len(self.data)):
+            self.keys.update({i:data[i]})
+
         
     def optimize(self):
         C = 0
         #get initial core points
         #for every point in data
-        for p in data:
+        for p in self.keys:
+            print("\n####################Point ",p," out of ", len(self.keys),"############################\n")
             #if point has a label, skip
-            if self.label.get(p) != None:
+            if self.labels.get(p) != None:
                 continue
             
             #get its neighbors
@@ -35,28 +42,42 @@ class DBSCAN():
             
             C =+1
             #give p label
-            self.labels.update({p:C}) #potential problem here with strings and ints in the dictionary
+            self.labels.update({p:C})
             seed = neighbors
+            new_neighbors = False
             while(new_neighbors==False):
+                
                 for Q in seed:
+                    print("Neighbor ", Q, " out of ", len(seed))
+                    #if the point was previously noise, give it the label of the current point
                     if self.labels.get(Q) == 'Noise':
+                        print("Noise")
                         self.labels.update({Q:C})
+                    #if the point was 
                     elif self.labels.get(Q) == None:
+                        print("None")
                         continue
                     else:
+                        print("Other")
                         self.labels.update({Q:C})
                         neighbors = self.range_query(Q)
                         if len(neighbors) >= self.min_pts:
                             seed.append(neighbors)
                             new_neighbors = True
-        return(self.labels)
+                            
+        return self.labels, self.keys
        
     #scans the data and finds the neighbors of a point
     def range_query(self,p):
         neighbors = []
-        for x in self.data:
+        for x in self.keys:
             if x != p:
-                if np.linalg.norm(p,x) <= self.eps:
+                x_V = self.keys.get(x)
+                p_V = self.keys.get(p)
+                #if the point is within epsilon from current point
+                if np.linalg.norm(p_V-x_V) <= self.eps:
+                    print("Found Neighbor")
+                    #add it to the neighbors list
                     neighbors.append(x)
         return neighbors
     
