1b19ffc1f0364c1bc16b6bb59a5998547de0c650
jakedanczyk <danczykj@gmail.com>
2017-12-10 21:58:00 -0700

    PSO final?

--- Project 4/src/PSO.py
+++ Project 4/src/PSO.py
@@ -4,46 +4,23 @@ import numpy as np
 import random
 
 class PSO():
-    
-    data = {}
-    agents = {}
-    fitness_scores = []
-    lbests = [] #holds subarrays  of lbest fitness and centroids for each particle
-    gbest = [] #fitness and centroids of gbest
-    inertia = 0
-    constriction = 0
-    grid = []
-    
-
-        #initialize each particle to contain Nc random centroids
-        #
-        #unti max_iter
-            #for each particle
-                #for each data vector
-                    #calculate euclidean distance to centroids
-                    #assign to closest centroid
-                    #calculate fitness
-                #update gbest and lbest
-                #update centroids
-
-    #
 
     def __init__(self, data, num_agents, num_centroids, inertia, c1,c2,max_iter):
-        self.agents = {}
+        self.agents = {}                            #key = agent_i, data = [[list of centroids],[lists of data vectors[]],[list of velocities]]
         self.fitness_scores = [None]*num_agents
-        self.lbests = [[9999999999,[]]]*num_agents #holds subarrays  of lbest fitness and centroids for each particle
+        self.lbests = [[9999999999,[]]]*num_agents  #holds subarrays  of lbest fitness and centroids for each particle
         self.gbest = [999999999,[]]
         self.data = data
         self.max_iter = max_iter
-        self.inertia = inertia
-        self.c1 = c1
-        self.c2 = c2
-        num_dims = len(next(iter(data)))
-        self.max_vals = next(iter(data))
-        self.max_vals = list(self.max_vals)
-        self.min_vals = next(iter(data))
+        self.inertia = inertia                      #inertia value for velocity update
+        self.c1 = c1                                #weight for influence of local best position
+        self.c2 = c2                                #weight for influence of global best positon
+        num_dims = len(next(iter(data)))            #number of dimensions in dataset
+        self.max_vals = next(iter(data))            #largest values in each dimension present in dataset
+        self.max_vals = list(self.max_vals) 
+        self.min_vals = next(iter(data))            #smallest values in each dimension present in dataset
         self.min_vals = list(self.min_vals)
-        for data_key in data.keys():
+        for data_key in data.keys():                #finding min and max vals
             k = 0
             for feature in data_key:
                 if feature > self.max_vals[k]:
@@ -51,87 +28,72 @@ class PSO():
                 elif feature < self.min_vals[k]:
                     self.min_vals[k] = feature
                 k += 1
-        for i in range(num_agents):
+        for i in range(num_agents):                 #initialize agents. centroid position and velocities random but bounded by min and max vals
             new_agent_key = "agent" + str(i)
-            centroids = [None]*num_centroids #needs to be a dictionary, key is coords, value is list of data vectors assigned
+            centroids = [None]*num_centroids
             velocities = [[None]*num_dims]*num_centroids
             for k in range(num_centroids):
                 centroid = []
                 for j in range(num_dims):
                     centroid.append(random.uniform(self.min_vals[j], self.max_vals[j]))
-                    velocities[k][j] = (0.01*random.uniform(self.min_vals[j], self.max_vals[j]))
-                    #print(k)
-                    #print(velocities[k])
+                    velocities[k][j] = (0.001*random.uniform(self.min_vals[j], self.max_vals[j]))
                 centroids[k] = centroid
             self.agents[new_agent_key] = [centroids,[[]]*num_centroids,velocities]
-    
+     
 
-    def velocity(self):
-        pass
-    
-    #agents is dictionary of agents: key = agent_i, data = [[list of centroids],[lists of data vectors[]],[list of velocities]]
-
+    #optimization loop. runs for max_iter times
     def optimize(self):
         for iter in range(self.max_iter):
             i = -1
-            for agent in self.agents:
-                #assign data points to nearest centroid
+            for agent in self.agents: 
                 i+=1
-                for x in range(len(self.agents[agent][1])):
+                for x in range(len(self.agents[agent][1])): #clear lists of assigned data points 
                     self.agents[agent][1][x] = []
-                for data_key in self.data.keys():
+                for data_key in self.data.keys(): #assign data points to nearest centroid
                     distances = []
                     for centroid in self.agents[agent][0]:
-                        distances.append(np.linalg.norm(np.array(list(data_key)) - np.array(centroid)))
+                        dist = np.linalg.norm(np.array(list(data_key)) - np.array(centroid))
+                        distances.append(dist)
                     min_dist = min(distances)
-                    self.agents[agent][1][distances.index(min_dist)].append([data_key,min_dist])
-                #calculate fitness
+                    self.agents[agent][1][distances.index(min_dist)].append([data_key,min_dist,self.data[data_key]]) 
+                #calculate fitness for this agent
                 fitness = 0
                 j = -1
                 sum = 0
                 for centroid in self.agents[agent][0]:
                     j+=1
                     size = len(self.agents[agent][1][j])
-                    for data_vector in self.agents[agent][1][j]:
-                        sum += data_vector[1]/size #((np.linalg.norm(np.array(list(data_vector)) - np.array(centroid)))/size)
-                if sum == 0:
-                    print(sum)
-                fitness = sum / len(self.agents[agent][0])
-                if fitness == 0:
-                    print("0 fitness")
-                    #print(self.agents[agent][1])
+                    for data_vector in self.agents[agent][1][j]:        #for each data point, add distance divided by centroid population count to sum
+                        sum += data_vector[1]/size 
+                fitness = sum / len(self.agents[agent][0])              #weight fitness by number of centroids
                 self.fitness_scores[i] = fitness
-                if fitness < self.lbests[i][0]:
-                    print('new lbest')
-                    self.lbests[i] = [fitness, self.agents[agent][0]]
-                    if fitness < self.gbest[0]:
+                if fitness < self.lbests[i][0]:                         #update local best if previous exceeded, save centroid positions
+                    self.lbests[i] = [fitness, self.agents[agent][0]]   
+                    if fitness < self.gbest[0]:                         #update global best if previous exceeded, save centroid positions and data point centroid assigments
                         print('new gbest')
                         self.gbest = [fitness, self.agents[agent][0], self.agents[agent][1]]
-                #update cluster velocity
                 j = 0
-                for velocity in self.agents[agent][2]:
+                for velocity in self.agents[agent][2]:                  #update cluster velocity = inertia*velocity + c1*random(0,1)*(lbest-present) + c2*random(0,1)*(gbest-present)
                     for x in range(len(velocity)):
                        self.agents[agent][2][j][x] += self.inertia * velocity[x]
                        self.agents[agent][2][j][x] += self.c1*random.uniform(0,1)*(self.lbests[0][1][j][x] - self.agents[agent][0][j][x])
                        self.agents[agent][2][j][x] += self.c2*random.uniform(0,1)*(self.gbest[1][j][x] - self.agents[agent][0][j][x])
                     j += 1
-                #update cluster position    
                 j=0
-                for centroid in self.agents[agent][0]:
+                for centroid in self.agents[agent][0]:                  #update cluster position = present + velocity
                     for x in range(len(centroid)):
                         self.agents[agent][0][j][x] += self.agents[agent][2][j][x]
                     j+=1
-            print(self.fitness_scores)
             print(self.gbest[0])
-        gbest_clusters = {}
+        gbest_clusters = {}                                             #create dictionary of final gbest clusters for data processing
         for i in range(len(self.gbest[2])):
-            key = "cluster_"+str(i)
-            gbest_clusters[key] = self.gbest[2][i]
+            if len(self.gbest[2][i]) > 0:
+                key = "cluster_"+str(i)
+                print(self.gbest[2][i])
+                points = []
+                for x in self.gbest[2][i]:
+                    points.append(np.array(list(x[0])))
+                gbest_clusters[key] = points
+            else:
+                pass
         return gbest_clusters
-    
-
-    def output(self):
-        pass
-    
-    def update_position(self):
-        pass
